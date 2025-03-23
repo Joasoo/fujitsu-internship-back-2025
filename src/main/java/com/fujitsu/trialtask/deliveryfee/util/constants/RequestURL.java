e2e7047aae76a12a725c9998a2d2ae26c2a529fe
Markus Joasoo <majoas@taltech.ee>
2025-03-23 22:03:25 +0200

    Remove unnecessary files

--- src/main/java/com/fujitsu/trialtask/deliveryfee/util/constants/RequestURL.java
+++
@@ -1,9 +0,0 @@
-package com.fujitsu.trialtask.deliveryfee.util.constants;
-
-/**
- * Request URL constants.
- */
-public class RequestURL {
-    private RequestURL(){}
-    public static final String  WEATHER_OBSERVATION = "https://www.ilmateenistus.ee/ilma_andmed/xml/observations.php";
-}
